f4080b470a23a9df4bc57919b2b3ef000bc390b3
Jason McKenzie <jmac.atx@gmail.com>
2021-08-04 15:47:55 -0500

    adds users and songs to seeds, create users and songs actions

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,17 @@
 class SongsController < ApplicationController
+  def create
+    song = Song.new(
+      song_name: params[:song_name],
+      artist_name: params[:artist_name],
+      album_name: params[:album_name],
+      album_art_url: params[:album_art_url],
+      song_url: params[:song_url],
+      buy_link: params[:buy_link],
+    )
+    if song.save
+      render json: { message: "Song created successfully" }, status: :created
+    else
+      render json: { errors: song.errors.full_messages }, status: :bad_request
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user = User.create!(user_name: "Joe Blow", email: "joeblow@email.com", password_digest: "password")
-user = User.create!(user_name: "Jane Doe", email: "janedoe@email.com", password_digest: "password")
-user = User.create!(user_name: "Robert Though", email: "robertthough@email.com", password_digest: "password")
+# user = User.create!(user_name: "Joe Blow", email: "joeblow@email.com", password_digest: "password")
+# user = User.create!(user_name: "Jane Doe", email: "janedoe@email.com", password_digest: "password")
+# user = User.create!(user_name: "Robert Though", email: "robertthough@email.com", password_digest: "password")
+
+song = Song.create!(song_name: "Song 1", artist_name: "Artist 1", album_name: "Album 1", album_art_url: "arturl1.jpg", song_url: "song1url", buy_link: "buysong1")
+song = Song.create!(song_name: "Song 2", artist_name: "Artist 1", album_name: "Album 1", album_art_url: "arturl2.jpg", song_url: "song2url", buy_link: "buysong2")
+song = Song.create!(song_name: "Song 3", artist_name: "Artist 2", album_name: "Album 2", album_art_url: "arturl3.jpg", song_url: "song3url", buy_link: "buysong3")
+song = Song.create!(song_name: "Song 4", artist_name: "Artist 3", album_name: "Album 3", album_art_url: "arturl4.jpg", song_url: "song4url", buy_link: "buysong4")
+song = Song.create!(song_name: "Song 5", artist_name: "Artist 3", album_name: "Album 4", album_art_url: "arturl5.jpg", song_url: "song5url", buy_link: "buysong5")
+song = Song.create!(song_name: "Song 6", artist_name: "Artist 4", album_name: "Album 5", album_art_url: "arturl6.jpg", song_url: "song6url", buy_link: "buysong6")
+song = Song.create!(song_name: "Song 7", artist_name: "Artist 4", album_name: "Album 5", album_art_url: "arturl7.jpg", song_url: "song7url", buy_link: "buysong7")
