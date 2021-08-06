9bb69881f427bd82cf5ae53c94c61850832f7a46
Jason McKenzie <jmac.atx@gmail.com>
2021-08-06 12:21:55 -0500

    adds routes for playlist index/show/update, songs update

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,27 @@ Rails.application.routes.draw do
 
   # users routes
 
+  # get "/users" => "users#index"
+  # get "users/:id" => "users#show"
   post "/users" => "users#create"
+  # patch "users/:id" => "users#update"
+  # delete "users/:id" => "users#destroy"
 
   # songs routes
 
+  # get "/songs" => "songs#index"
+  # get "songs/:id" => "songs#show"
   post "/songs" => "songs#create"
+  patch "songs/:id" => "songs#update"  # updates status on playlist?
+  # delete "songs/:id" => "songs#destroy"
 
   # playlists routes
 
+  get "/playlists" => "playlists#index"
+  get "playlists/:id" => "playlists#show"
   post "/playlists" => "playlists#create"
+  patch "playlists/:id" => "playlists#update" # updates details including active status
+  # delete "playlists/:id" => "playlists#destroy"
 
   # sessions routes
 
