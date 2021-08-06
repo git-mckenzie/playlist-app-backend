74a4c841adabac1ae7283b447cd18c20fffd1224
Jason McKenzie <jmac.atx@gmail.com>
2021-08-06 12:09:43 -0500

    adds sessions controller and post routes for users,songs,playlists,sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # users routes
+
+  post "/users" => "users#create"
+
+  # songs routes
+
+  post "/songs" => "songs#create"
+
+  # playlists routes
+
+  post "/playlists" => "playlists#create"
+
+  # sessions routes
+
+  post "/sessions" => "sessions#create"
 end
