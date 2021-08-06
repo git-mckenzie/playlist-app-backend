5136bd9d0afdbee57bf8d2fa04b293ea98687be6
Jason McKenzie <jmac.atx@gmail.com>
2021-08-06 12:32:46 -0500

    adds index, show and update to playlists controller

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,4 +1,15 @@
 class PlaylistsController < ApplicationController
+  def index
+    playlist = Playlist.all
+    render json: playlist
+  end
+
+  def show
+    playlist_id = params["id"]
+    playlist = Playlist.find(playlist_id)
+    render json: playlist
+  end
+
   def create
     playlist = Playlist.new(
       name: params[:name],
@@ -9,6 +20,19 @@ class PlaylistsController < ApplicationController
       render json: { message: "Playlist created successfully" }, status: :created
     else
       render json: { errors: playlist.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  def update
+    playlist_id = params["id"]
+    playlist = Playlist.find(playlist_id)
+
+    playlist.name = params["name"] || playlist.name
+
+    if playlist.save
+      render json: playlist
+    else
+      render json: { errors: playlist.errors.full_messages }, status: 418
     end
   end
 end
