fe318bc4fcc71393297594534e5e3636fa9ee644
Jason McKenzie <jmac.atx@gmail.com>
2021-08-06 15:43:20 -0500

    adds user update route and method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,4 +12,20 @@ class UsersController < ApplicationController
       render json: { errors: user.errors.full_messages }, status: :bad_request
     end
   end
+
+  def update
+    user_id = params["id"]
+    user = User.find(user_id)
+
+    user.user_name = params["user_name"] || user.user_name
+    user.email = params["email"] || user.email
+    user.password = params["password"] || user.password
+    user.password_confirmation = params["password_confirmation"] || user.password_confirmation
+
+    if user.save
+      render json: user
+    else
+      render json: { errors: user.errors.full_messages }, status: 418
+    end
+  end
 end
