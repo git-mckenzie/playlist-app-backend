eafa530eaa00a754297386a450ace1b5c9486438
Jason McKenzie <jmac.atx@gmail.com>
2021-08-10 13:29:20 -0500

    cleans up playlist update and changes create to current_user id

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -14,7 +14,7 @@ class PlaylistsController < ApplicationController
 
   def create
     playlist = Playlist.new(
-      user_id: params[:user_id],
+      user_id: current_user.id,
       playlist_name: params[:playlist_name],
     )
     if playlist.save
@@ -26,21 +26,10 @@ class PlaylistsController < ApplicationController
 
   def update
     playlist_id = params[:id]
-    playlist_id_int = playlist_id.to_i
-    playlist = Playlist.find(playlist_id_int)
-    user_playlist = playlist.user_id.to_i
-    puts playlist_id
-    puts playlist_id_int
-    puts current_user.id
-    puts playlist.user_id
-    puts playlist.user_id.class
-    # render json: { message: playlist.user_id }
-    # render json: { message: current_user.id }
+    playlist = Playlist.find(playlist_id)
+    playlist_user_id = playlist.user_id
 
-    if user_playlist == current_user.id
-      # render json: { message: playlist.user_id }
-      # render json: { message: current_user.id }
-
+    if playlist_user_id == current_user.id
       playlist.playlist_name = params[:playlist_name] || playlist.playlist_name
 
       if playlist.save
