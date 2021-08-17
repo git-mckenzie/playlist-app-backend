0e6cb348e0f8f2e7e47074ae3214f6c86699600d
Jason McKenzie <jmac.atx@gmail.com>
2021-08-17 12:05:27 -0500

    adds routes for users index and show

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
 
   # users routes
 
-  # get "/users" => "users#index"
-  # get "users/:id" => "users#show"
+  get "/users" => "users#index"
+  get "users/:id" => "users#show"
   post "/users" => "users#create"
   patch "users/:id" => "users#update"
   # delete "users/:id" => "users#destroy"
